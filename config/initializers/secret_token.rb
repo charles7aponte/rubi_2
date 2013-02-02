98be2a978ad3dfeab80d2b0ca032d5c628c413ae
charles <charles7aponte@gmail.com>
2013-02-02 05:29:45 -0600

    primer guarda

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubi2::Application.config.secret_token = '948839cfba9c8f36c6dabf9879571579602f9406993863a86bed8c1b5e70c469ff4f27a3d463e0904f10b785272cf1a7c6b454ea9fd8a6b72a0bb2fa260b7aff'
